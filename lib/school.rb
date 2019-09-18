d461d35809db5bb3851498303796215a4ab4630a
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-18 00:33:50 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
-# code here!
+class School 
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  
+  def roster 
+    @roster
+  end
+  
+  
+  def add_student(name, grade) 
+    @roster[grade] = [] unless @roster.include?(grade)
+    @roster[grade] << name
+  end 
+  
+  
+  def grade(grade)
+    @roster[grade]
+  end 
+  
+  
+  def sort
+    
+    sorted_roster = {}
+    
+    @roster.collect do |grade, student_array|
+      sorted_roster[grade] = student_array.sort
+    end 
+    
+    sorted_roster
+    
+  end 
+  
+end 
+
